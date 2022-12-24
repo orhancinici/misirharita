552cdbf98c3f0ccc4d1c0b9fd58e6a6b50c14298
Orhan Cinici <75792549+orhancinici@users.noreply.github.com>
2022-12-24 21:29:03 +0300

    Add files via upload

---
+++ data/camiler.js
@@ -0,0 +1,117 @@
+var camiJSON = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Camiu'l-Attarin / Camiu’l-Cüyuşi",
+          "arabicName": "مسجد العطارين"
+        },
+        "geometry": {
+          "coordinates": [
+            29.89798773333098,
+            31.194914199869302
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "İbn Tolun Camii",
+          "arabicName": "مسجد ابن طولون"
+        },
+        "geometry": {
+          "coordinates": [
+            31.249545648893843,
+            30.028761581479316
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Amr b. el-As Camii",
+          "arabicName": "مسجد عمرو بن العاص"
+        },
+        "geometry": {
+          "coordinates": [
+            31.23313971926237,
+            30.010479942287816
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Ezher Camii",
+          "arabicName": "الجامع الازهر"
+        },
+        "geometry": {
+          "coordinates": [
+            31.262727182434986,
+            30.04583267624608
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Hakim Bi-Emrillah Camii",
+          "arabicName": "مسجد حاكم بامر الله"
+        },
+        "geometry": {
+          "coordinates": [
+            31.26379304490561,
+            30.054600881418935
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Akmer Camii",
+          "arabicName": "مسجد الأقمر"
+        },
+        "geometry": {
+          "coordinates": [
+            31.262092311315882,
+            30.051499869928207
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Salih Talai Camii",
+          "arabicName": "مسجد الصالح طلائع"
+        },
+        "geometry": {
+          "coordinates": [
+            31.25794689885035,
+            30.042354469751174
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "el-Camiu’z-Zafir/Camiu'l-Fukhani",
+          "arabicName": "جامع الفكهاني"
+        },
+        "geometry": {
+          "coordinates": [
+            31.259066201580453,
+            30.04446562664701
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
---
+++ data/location.js
@@ -0,0 +1,294 @@
+var locationMisirJSON = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kahire",
+        "arabicName": "القاهرة",
+        "img": "kahire.jpg",
+        "doc": "kahire.pdf",
+        "bilgi": "..."
+      },
+      "geometry": {
+        "coordinates": [
+          31.23855212778963,
+          30.04149953871554
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Fustat",
+        "arabicName": "الفسطاط",
+        "img": "fustat.jpg",
+        "doc": "fustat.pdf",
+        "bilgi": "..."
+      },
+      "geometry": {
+        "coordinates": [
+          31.236121535554815,
+          30.009986285902613
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Cize",
+        "arabicName": "الجيزة"
+      },
+      "geometry": {
+        "coordinates": [
+          31.21178677583788,
+          29.985434799037748
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Feyyum",
+        "arabicName": "الفيوم",
+        "img": "feyyum.jpg",
+        "doc": "feyyum.pdf",
+        "bilgi": "..."
+      },
+      "geometry": {
+        "coordinates": [
+          30.83954642019799,
+          29.319276438088252
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "İskenderiye",
+        "arabicName": "الإسكندرية"
+      },
+      "geometry": {
+        "coordinates": [
+          29.896398190965243,
+          31.18927186156054
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Hulvan",
+        "arabicName": "حلوان"
+      },
+      "geometry": {
+        "coordinates": [
+          31.3341420767712,
+          29.845334355388644
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Dimyat",
+        "arabicName": "دمياط"
+      },
+      "geometry": {
+        "coordinates": [
+          31.81545366229787,
+          31.415441586913957
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Asyut",
+        "arabicName": "أسيوط"
+      },
+      "geometry": {
+        "coordinates": [
+          31.187140587961636,
+          27.18128358008964
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kıft",
+        "arabicName": "قفط"
+      },
+      "geometry": {
+        "coordinates": [
+          32.81739586783064,
+          26.002395296747494
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kus",
+        "arabicName": "القوصة"
+      },
+      "geometry": {
+        "coordinates": [
+          32.763012850503856,
+          25.91416911029569
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "İsna",
+        "arabicName": "إسنا"
+      },
+      "geometry": {
+        "coordinates": [
+          32.55028383504765,
+          25.2845343741419
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Udfüv",
+        "arabicName": "إدفو"
+      },
+      "geometry": {
+        "coordinates": [
+          32.869959836793186,
+          24.985404363106028
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Esvan",
+        "arabicName": "أسوان"
+      },
+      "geometry": {
+        "coordinates": [
+          32.91105842548703,
+          24.088715387333224
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Raşid",
+        "arabicName": "رشيد"
+      },
+      "geometry": {
+        "coordinates": [
+          30.418527803345427,
+          31.411864889720633
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Kalzüm/Kulzüm",
+        "arabicName": "القلزم"
+      },
+      "geometry": {
+        "coordinates": [
+          32.53521126472532,
+          29.973684476546282
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "İhmim",
+        "arabicName": "أخميم"
+      },
+      "geometry": {
+        "coordinates": [
+          31.74339800907501,
+          26.56345576373502
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Tinnis",
+        "arabicName": "تنيس"
+      },
+      "geometry": {
+        "coordinates": [
+          32.28486056626866,
+          31.26277883455596
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Busir",
+        "arabicName": "أبو صير"
+      },
+      "geometry": {
+        "coordinates": [
+          31.21925621273519,
+          29.889912416256706
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Behnesa?",
+        "arabicName": "البهنسا"
+      },
+      "geometry": {
+        "coordinates": [
+          30.665371878433263,
+          28.534565050871407
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Ferama",
+        "arabicName": "الفرما"
+      },
+      "geometry": {
+        "coordinates": [
+          32.5913796066647,
+          31.049305916697307
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
